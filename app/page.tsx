88cf67298b799bfb93832f0b68d3bba0a5fd247a
IRADUKUNDA Allelua Fiacre <firaduk@yahoo.com>
2024-06-03 12:06:43 +0200

    Improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import LogoComponent from "@/components/logo/LogoComponent";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <LogoComponent />
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <h1>Welcome to Driver hub</h1>
       </div>
