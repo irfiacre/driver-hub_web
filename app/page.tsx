da7ca3042be3b84d4ca76f79682241a5dc14ca6c
IRADUKUNDA Allelua Fiacre <firaduk@yahoo.com>
2024-06-03 11:49:46 +0200

    Initial changes

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+        <h1>Welcome to Driver hub</h1>
+      </div>
+    </main>
+  );
+}
